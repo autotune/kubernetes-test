372b167fe03bcf410a358422f18617979e43e8e9
Brian Adams <service@contrasting.org>
2021-06-12 02:28:56 -0500

    Added ecr

--- terraform/production/backend.tf
+++ terraform/production/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket     = "badams"
     key        = "badams.tfstate"
     region     = "us-east-1"
-    access_key = "$AWS_ACCESS_KEY_ID"
-    secret_key = "$AWS_SECRET_ACCESS_KEY"
+    access_key = ""
+    secret_key = ""
   }
 }
