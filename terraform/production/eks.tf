956199ddf192147573a9660c5548c62727e20620
Brian Adams <service@contrasting.org>
2021-06-12 15:49:36 -0500

    Added eks

--- terraform/production/eks.tf
+++ terraform/production/eks.tf
@@ -161,57 +161,10 @@ resource "aws_db_subnet_group" "badams" {
   }
 }
 
-
-# Use the last snapshot of the dev database before it was destroyed to create
-# a new dev database.
-resource "aws_rds_cluster" "aurora" {
-  cluster_identifier   = "badams-eks-prod"
-  snapshot_identifier  = aws_db_cluster_snapshot.badams.id
-  db_subnet_group_name = aws_db_subnet_group.badams.id
-  engine               = "aurora-postgresql"
-  engine_version       = "10.14"
-  engine_mode          = "serverless"
-
-  vpc_security_group_ids = [aws_security_group.badams.id]
-  scaling_configuration {
-    auto_pause               = true
-    max_capacity             = 8
-    min_capacity             = 2
-    seconds_until_auto_pause = 300
-    timeout_action           = "ForceApplyCapacityChange"
-  }
-
-  final_snapshot_identifier = "badams-eks-prod-final"
-
-  lifecycle {
-    ignore_changes = [snapshot_identifier]
-  }
-
-  depends_on = [aws_db_cluster_snapshot.badams]
-}
-
-resource "aws_eip" "lb" {
-  count = 3
-  vpc   = true
-  tags = {
-    Terraform = "True"
-    Env       = "Prod"
-    App       = "badams"
-  }
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.default.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.default.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.default.token
-  }
 }
 
 resource "local_file" "kubeconfig" {
